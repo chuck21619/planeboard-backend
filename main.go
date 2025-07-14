a8afc68a0e5350c149de330c883e4854db20674b
Charles Johnston <charles.ward.johnston@gmail.com>
2025-07-13 22:52:49 -0400

    add report endpoint that emails me the message

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"net/smtp"
 	"os"
 	"time"
 
@@ -42,6 +44,9 @@ func main() {
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		ws.ServeWebSocket(hub, w, r)
 	})
+
+	http.HandleFunc("/report", withCORS(handleReport))
+
 	port := os.Getenv("PORT")
 
 	log.Printf("Server started on :%s", port)
@@ -85,4 +90,48 @@ func logRoomCountToCSV(filename string, roomCount int) {
 	} else {
 		log.Println("Logged room count to CSV.")
 	}
+}
+
+func handleReport(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var payload struct {
+		Message string `json:"message"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	if payload.Message == "" {
+		http.Error(w, "Empty message", http.StatusBadRequest)
+		return
+	}
+
+	err := SendEmail(payload.Message)
+	if err != nil {
+		log.Printf("Failed to send report: %v", err)
+		http.Error(w, "Failed to send report", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func SendEmail(body string) error {
+	address := os.Getenv("GMAIL_ADDRESS")
+	password := os.Getenv("GMAIL_APP_PASSWORD")
+	auth := smtp.PlainAuth("", address, password, "smtp.gmail.com")
+	message := []byte("Subject: " + "\r\n" + body)
+
+	err := smtp.SendMail("smtp.gmail.com:587", auth, address, []string{address}, message)
+	if err != nil {
+		return fmt.Errorf("failed to send email: %v", err)
+	}
+	return nil
 }
